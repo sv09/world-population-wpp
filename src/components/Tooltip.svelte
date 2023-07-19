f6149d70fe11767e954535abc1af5b7b5e23cac0
sv09 <shreyavinod09@gmail.com>
2023-07-19 00:01:23 -0400

    component to render tooltip

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let name;
+    export let population;
+    export let x;
+    export let y;
+</script>
+
+<div style=" top: {y+5}px; left: {x+5}px; ">Country: { name } <br>Population: { population } </div>
+
+<style>
+    div{
+        background: rgba(0,0,0,0.5);
+        color: rgb(255,255,255);
+        padding: 15px;
+        position: absolute;
+        font-size: 12px;
+    }
+</style>
